6c9aa41c7cde3aaa0c17fbecc779b092ec44576a
LeonelKim <48311273@est.ort.edu.ar>
2025-12-05 11:48:47 -0300

    Add files via upload

--- proyecto_dog.ino
+++ proyecto_dog.ino
@@ -1,12 +1,16 @@
+//GRUPO 3 KIM,HOROWITZ,MORAT,WIRZEYLO
+
+
 //-----BOTONES----
 #define BOTON_UP 25
 #define BOTON_DOWN 26
 #define BOTON_LEFT 27
-#define BOTON_RIGHT 32
+#define BOTON_RIGHT 35
 #define BOTON_OK 13
 
-#define MQ4_PIN 34
-#define MQ9_PIN 35
+#define MQ4_PIN 32
+#define MQ9_PIN 33
+#define LDR 34
 
 //------MAQUINA DE ESTADOS------
 #define P0 50
@@ -47,10 +51,13 @@
 #include <WiFiClientSecure.h>
 #include <UniversalTelegramBot.h>
 #include <Preferences.h>
+#include "AsyncMqttClient.h"
+#include "time.h"
+#include "Arduino.h"
 
 
 //------VARIABLES------
-int estadoPantalla = P0;
+int estadoPantalla = P1;
 int opcionSeleccionada = 1;  // opción actual del menú (1 a 5)
 int umbralTH = 30;
 int umbralHUM = 55;
@@ -63,8 +70,8 @@ int gmt = 0;
 int rawValue;
 int lightPercent;
 unsigned long ultimoRefreshHora = 0;
-const char* ssid = "OSONDOSON 2.4Ghz";
-const char* password = "O46334737";
+const char* ssid = "Fibertel2025";
+const char* password = "Diciembre2026$";
 bool pantallaMostrada = false;
 int intervaloMqtt = 30;
 unsigned long ultimoTelegram = 0;
@@ -86,6 +93,208 @@ int MQ9Mapped;
 bool alertaMQ4Enviada = false;
 bool alertaMQ9Enviada = false;
 
+const char name_device = 13;  ////device numero de grupo 5A 1x siendo x el numero de grupo
+///                        5B 2x siendo x el numero de grupo
+/// 5a grupo 4 => 14
+/// 5b grupo 3 => 23
+// Timers auxiliar variables//////////////////////////
+unsigned long now = millis();    ///valor actual
+unsigned long lastMeasure1 = 0;  ///variable para contar el tiempo actual
+unsigned long lastMeasure2 = 0;  ///variable para contar el tiempo actual
+
+unsigned long interval_envio = 30000;  // ahora será dinámico
+  //Intervalo de envio de datos mqtt
+const unsigned long interval_leeo = 60000;   //Intervalo de lectura de datos y guardado en la cola
+int i = 0;
+
+///time
+long unsigned int timestamp;  // hora
+const char* ntpServer = "south-america.pool.ntp.org";
+const long gmtOffset_sec = -10800;
+const int daylightOffset_sec = 0;
+
+///variables ingresar a la cola struct
+int indice_entra = 0;
+int indice_saca = 0;
+bool flag_vacio = 1;
+
+/////mqqtt
+#define MQTT_HOST IPAddress(192, 168, 5, 123)  ///se debe cambiar por el ip de meca o hall del 4
+#define MQTT_PORT 1884
+#define MQTT_USERNAME "esp32"
+#define MQTT_PASSWORD "mirko15"
+char mqtt_payload[150];  /////
+// Test MQTT Topic
+#define MQTT_PUB "/esp32/datos_sensores"
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
+
+typedef struct
+{
+  long time;
+  float T1;  ///temp en grados
+  float H1;  ///valor entre 0 y 99 // mojado es cercano al 100
+  float luz;  ///valor entre 0 y 99 . si hay luz es cercano al 100
+  float G1; ///valor entre 0 y 99
+  float G2; ///valor entre 0 y 99
+  bool oct; ///Lectura del octoacoplador
+  bool Alarma; //
+
+} estructura;
+////////////////
+const int valor_max_struct = 1000;          ///valor vector de struct
+estructura datos_struct[valor_max_struct];  ///Guardo valores hasta que lo pueda enviar
+estructura aux2;
+
+/////*********************************************************************/////
+////////////////////////////setup wifi/////////////////////////////////////////
+/////*********************************************************************/////
+void setupmqtt() {
+  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+  WiFi.onEvent(WiFiEvent);
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  mqttClient.setCredentials(MQTT_USERNAME, MQTT_PASSWORD);
+  connectToWifi();
+}
+////////////////////////////Envio de datos mqtt//////////////////////////////////////////
+////////Funcion que envia valores cuando la estructura no este vacia ///////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+void fun_envio_mqtt() {
+  fun_saca();           ////veo si hay valores nuevos
+  if (flag_vacio == 0)  ////si hay los envio
+  {
+    Serial.print("enviando");
+    ////genero el string a enviar 1. 2.   3.   4.   5.   6.   7   8  9.       1.         2.        3.      4.       5.           6.   7.        8.        9
+    snprintf(mqtt_payload, 150, "%u&%ld&%.2f&%.2f&%.2f&%.2f&%.2f&%u&%u", name_device, aux2.time, aux2.T1, aux2.H1, aux2.luz, aux2.G1, aux2.G2, aux2.oct, aux2.Alarma);  //random(10,50)
+    aux2.time = 0;                                                                                                                                                   ///limpio valores
+    aux2.T1 = 0;
+    aux2.H1 = 0;
+    aux2.luz = 0;
+    aux2.G1 = 0;
+    aux2.G2 = 0;
+    aux2.oct = 0;
+    aux2.Alarma = 0;
+
+    Serial.print("Publish message: ");
+    Serial.println(mqtt_payload);
+    // Publishes Temperature and Humidity values
+    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB, 1, true, mqtt_payload);
+  } else {
+    Serial.println("no hay valores nuevos");
+  }
+}  ///////////////////////////////////////////////////
+
+///////////////////////////////////////////////////
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(ssid, password);
+}  ///////////////////////////////////////////////////
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}  ///////////////////////////////////////////////////
+
+void WiFiEvent(WiFiEvent_t event) {
+  Serial.printf("[WiFi-event] event: %d\n", event);
+  switch (event) {
+    case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+      connectToMqtt();
+      break;
+    case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+      Serial.println("WiFi lost connection");
+      xTimerStop(mqttReconnectTimer, 0);  // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+      xTimerStart(wifiReconnectTimer, 0);
+      break;
+  }
+}  ///////////////////////////////////////////////////
+////////////////NO TOCAR FUNCIONES MQTT///////////////////////////////////
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}  ///////////////////////////////////////////////////
+
+////////////////NO TOCAR FUNCIONES MQTT///////////////////////////////////
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+  if (WiFi.isConnected()) {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
+}
+////////////////NO TOCAR FUNCIONES MQTT///////////////////////////////////
+void onMqttPublish(uint16_t packetId) {
+  Serial.print("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}  ///////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////
+/////////////Funcion que saca un valor de la estructura para enviar //////
+///////////////////////////////////////////////////////////////////////
+void fun_saca() {
+  if (indice_saca != indice_entra) {
+    aux2.time = datos_struct[indice_saca].time;
+    aux2.T1 = datos_struct[indice_saca].T1;
+    aux2.H1 = datos_struct[indice_saca].H1;
+    aux2.luz = datos_struct[indice_saca].luz;
+    aux2.G1 = datos_struct[indice_saca].G1;
+    aux2.G2 = datos_struct[indice_saca].G2;
+    aux2.oct = datos_struct[indice_saca].oct;
+    aux2.Alarma = datos_struct[indice_saca].Alarma;
+
+    flag_vacio = 0;
+
+    Serial.println(indice_saca);
+    if (indice_saca >= (valor_max_struct - 1)) {
+      indice_saca = 0;
+    } else {
+      indice_saca++;
+    }
+    Serial.print("saco valores de la struct isaca:");
+    Serial.println(indice_saca);
+  } else {
+    flag_vacio = 1;  ///// no hay datos
+  }
+  return;
+}
+/////////////////////////////////////////////////////////////////////
+/////////////funcion que ingresa valores a la cola struct///////////
+///////////////////////////////////////////////////////////////////
+void fun_entra(void) {
+  if (indice_entra >= valor_max_struct) {
+    indice_entra = 0;  ///si llego al maximo de la cola se vuelve a cero
+  }
+  //////////// timestamp/////// consigo la hora
+  Serial.print("> NTP Time:");
+  timestamp = time(NULL);
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return;  //// si no puede conseguir la hora
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  ///////////////////////// fin de consigo la hora
+  datos_struct[indice_entra].time = timestamp;
+  datos_struct[indice_entra].T1 = temperatura;  /// leeo los datos //aca va la funcion de cada sensor
+  datos_struct[indice_entra].H1 = humedadMapped;  //// se puede pasar por un parametro valor entre 0 y 100
+  datos_struct[indice_entra].luz = lightPercent;
+  datos_struct[indice_entra].G1 = MQ4Mapped;
+  datos_struct[indice_entra].G2 = MQ9Mapped;
+  datos_struct[indice_entra].oct = true;
+  datos_struct[indice_entra].Alarma = 1;
+
+  indice_entra++;
+  Serial.print("saco valores de la struct ientra");
+  Serial.println(indice_entra);
+}
 
 
 //-----OBJETOS------
@@ -247,17 +456,13 @@ void guardarConfig() {
 
 // -------- CONFIGURACIÓN INICIAL --------
 void setup() {
-  Wire.begin(21, 22);
-  Wire1.begin(18, 19);  //sda = 18 scl = 19
+  Wire.begin(21,22);
   lcd.begin(16, 2);
   lcd.backlight();
-  if (!aht.begin(&Wire1)) {
-    Serial.println("No se pudo encontrar el AHTX0. Verifica las conexiones SDA/SCL.");
-    while (1) delay(10);
-  }
-  Serial.println("Sensor AHTX0 detectado correctamente.");
 
   Serial.begin(115200);
+  //Setup de time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
   prefs.begin("config", true);  // Modo solo lectura para cargar
 
   umbralTH = prefs.getInt("umbralTH", 30);
@@ -267,19 +472,17 @@ void setup() {
   umbralLDR = prefs.getInt("umbralLDR", 50);
   gmt = prefs.getInt("gmt", 0);
   intervaloMqtt = prefs.getInt("mqtt_int", 30);
+  interval_envio = intervaloMqtt * 1000;
 
   prefs.end();
-  Serial.print("Conectando a ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  // Espera hasta conectarse
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
-  Serial.println("Conectado a WiFi!");
+  setupmqtt();
+ 
   client.setInsecure();  // Necesario para usar Telegram
+   if (!aht.begin()) {
+  Serial.println("❌ AHT20 NO detectado");
+} else {
+  Serial.println("✔ AHT20 detectado");
+}
 
 
 
@@ -288,6 +491,7 @@ void setup() {
   pinMode(BOTON_LEFT, INPUT_PULLUP);
   pinMode(BOTON_RIGHT, INPUT_PULLUP);
   pinMode(BOTON_OK, INPUT_PULLUP);
+  pinMode(LDR,INPUT);
 
 
   actualizarHoraGMT();
@@ -359,7 +563,7 @@ void TaskTelegram(void* pvParameters) {
             }
             String msg1 = "📊 *VALOR ACTUAL LUZ Y UMBRAL*\n\n";
 
-            msg1 += "💡 *Luz actual:* " + String(lightPercent, 1) + "%\n";
+            msg1 += "💡 *Luz actual:* " + String(lightPercent) + "%\n";
             msg1 += "🔸 *Umbral luz:* " + String(umbralLDR) + "%\n";
             msg1 += "📌 *Estado:* " + estadoLUZ + "\n\n";
 
@@ -493,6 +697,16 @@ void TaskTelegram(void* pvParameters) {
 // -------- LÓGICA PRINCIPAL --------
 void TaskPrincipal(void* pvParameters) {
   while (true) {
+    now = millis();
+    if (now - lastMeasure1 > interval_envio) {  ////envio el doble de lectura por si falla algun envio
+      lastMeasure1 = now;                       /// cargo el valor actual de millis
+      fun_envio_mqtt();                         ///envio los valores por mqtt
+    }
+    if (now - lastMeasure2 > interval_leeo) {
+      lastMeasure2 = now;  /// cargo el valor actual de millis
+      fun_entra();         ///ingreso los valores a la cola struct
+    }
+
     sensors_event_t humidity, temp;
     aht.getEvent(&humidity, &temp);
 
@@ -503,14 +717,16 @@ void TaskPrincipal(void* pvParameters) {
     // Mapear la humedad a 0–100 %
     humedadMapped = map((int)humedadRaw, 0, 100, 0, 100);
 
-    //rawValue = analogRead(LDR);
-    //lightPercent = map(rawValue, 0, 4095, 0, 100);
+    rawValue = analogRead(LDR);
+    lightPercent = map(rawValue, 0, 4095, 0, 100);
 
     valorMQ4 = analogRead(MQ4_PIN);
     MQ4Mapped = map(valorMQ4, 0, 4095, 0, 100);
 
     valorMQ9 = analogRead(MQ9_PIN);
     MQ9Mapped = map(valorMQ9, 0, 4095, 0, 100);
+
+
 
 
     switch (estadoPantalla) {
@@ -628,7 +844,7 @@ void TaskPrincipal(void* pvParameters) {
         if (digitalRead(BOTON_LEFT) == HIGH) {
           if (selectorTH == 0 && umbralTH < 100) umbralTH++;
           if (selectorTH == 1 && umbralHUM < 100) umbralHUM++;
-           guardarConfig();
+          guardarConfig();
           estadoPantalla = P1;
           pantallaMostrada = false;
         }
@@ -639,7 +855,7 @@ void TaskPrincipal(void* pvParameters) {
         if (digitalRead(BOTON_RIGHT) == HIGH) {
           if (selectorTH == 0 && umbralTH > 0) umbralTH--;
           if (selectorTH == 1 && umbralHUM > 0) umbralHUM--;
-           guardarConfig();
+          guardarConfig();
           estadoPantalla = P1;
           pantallaMostrada = false;
         }
@@ -692,7 +908,7 @@ void TaskPrincipal(void* pvParameters) {
       case SUMA_LDR:
         if (digitalRead(BOTON_LEFT) == HIGH) {
           if (umbralLDR < 100) umbralLDR++;
-          guardarConfig();   
+          guardarConfig();
           estadoPantalla = P2;
           pantallaMostrada = false;
         }
@@ -702,7 +918,7 @@ void TaskPrincipal(void* pvParameters) {
       case RESTA_LDR:
         if (digitalRead(BOTON_RIGHT) == HIGH) {
           if (umbralLDR > 0) umbralLDR--;
-          guardarConfig();   
+          guardarConfig();
           estadoPantalla = P2;
           pantallaMostrada = false;
         }
@@ -769,7 +985,7 @@ void TaskPrincipal(void* pvParameters) {
         if (digitalRead(BOTON_LEFT) == HIGH) {
           if (selectorGAS == 0 && umbralMQ4 < 100) umbralMQ4++;
           if (selectorGAS == 1 && umbralMQ9 < 100) umbralMQ9++;
-           guardarConfig();
+          guardarConfig();
           estadoPantalla = P3;
           pantallaMostrada = false;
         }
@@ -780,7 +996,7 @@ void TaskPrincipal(void* pvParameters) {
         if (digitalRead(BOTON_RIGHT) == HIGH) {
           if (selectorGAS == 0 && umbralMQ4 > 0) umbralMQ4--;
           if (selectorGAS == 1 && umbralMQ9 > 0) umbralMQ9--;
-           guardarConfig();
+          guardarConfig();
           estadoPantalla = P3;
           pantallaMostrada = false;
         }
@@ -857,7 +1073,7 @@ void TaskPrincipal(void* pvParameters) {
 
 
 
-        //-------TELEGRAM, MQTT---------
+      //-------TELEGRAM, MQTT---------
       case ESPERA5:
         if (digitalRead(BOTON_OK) == HIGH) {
           delay(100);
@@ -889,6 +1105,7 @@ void TaskPrincipal(void* pvParameters) {
       case RESTA_INT:  // RESTA_INTERVALO
         if (digitalRead(BOTON_RIGHT) == HIGH) {
           if (intervaloMqtt > 30) intervaloMqtt -= 30;
+          interval_envio = intervaloMqtt * 1000;
           guardarConfig();
           estadoPantalla = P5;
           pantallaMostrada = false;
@@ -899,6 +1116,7 @@ void TaskPrincipal(void* pvParameters) {
       case SUMA_INT:  // SUMA_INTERVALO
         if (digitalRead(BOTON_LEFT) == HIGH) {
           if (intervaloMqtt < 3600) intervaloMqtt += 30;
+          interval_envio = intervaloMqtt * 1000;
           guardarConfig();
           estadoPantalla = P5;
           pantallaMostrada = false;
